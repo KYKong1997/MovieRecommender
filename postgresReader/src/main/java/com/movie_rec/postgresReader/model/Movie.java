1cbe1585892f13cb11915b93a02ea9d6a4f76058
KYKong1997 <kuokyong97@outlook.com>
2020-04-22 20:44:23 +0800

    Added frontend

--- postgresReader/src/main/java/com/movie_rec/postgresReader/model/Movie.java
+++ postgresReader/src/main/java/com/movie_rec/postgresReader/model/Movie.java
@@ -70,10 +70,30 @@ public class Movie extends AuditModel{
         this.rating = rating;
     }
 
-    @Column(name = "")
+    @Column(name = "movie_poster", nullable = false)
     public String getMovie_poster() {
         return movie_poster;
     }
 
     public void setMovie_poster(String movie_poster) {
-        this.movie_p
+        this.movie_poster = movie_poster;
+    }
+
+    @Column(name = "movie_url", nullable = false)
+    public String getMovie_url() {
+        return movie_url;
+    }
+
+    public void setMovie_url(String movie_url) {
+        this.movie_url = movie_url;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieId=" + movieId +
+                ", title='" + title + '\'' +
+                ", genres='" + genres + '\'' +
+                '}';
+    }
+}
